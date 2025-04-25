f51d545e63a37944e5b286add8f2351cd067ca9b
Lemul <igorsuvenir@gmail.com>
2025-04-25 22:08:36 +0700

    изменен скрипт

--- run_evaluator.ps1
+++ run_evaluator.ps1
@@ -2,9 +2,9 @@
 
 # Устанавливаем переменные окружения
 $env:LEADERBOARD_ROOT = "$PSScriptRoot\packages\leaderboard"
-$env:TEAM_AGENT = "$env:LEADERBOARD_ROOT\leaderboard\autoagents\human_agent.py"
-$env:ROUTES = "$env:LEADERBOARD_ROOT\data\routes_devtest.xml"
-$env:ROUTES_SUBSET = "0"
+$env:TEAM_AGENT = "team_code\autopilot_agent.py"
+$env:ROUTES = "$env:LEADERBOARD_ROOT\data\routes_training.xml"
+$env:ROUTES_SUBSET = "1" # номер сценария
 $env:REPETITIONS = "1"
 $env:DEBUG_CHALLENGE = "1"
 $env:CHALLENGE_TRACK_CODENAME = "SENSORS"
@@ -12,15 +12,15 @@ $env:CHECKPOINT_ENDPOINT = "$env:LEADERBOARD_ROOT\results.json"
 $env:RECORD_PATH = ""
 $env:RESUME = ""
 
+  # --routes-subset=$env:ROUTES_SUBSET `
+  # --repetitions=$env:REPETITIONS `
+  # --record=$env:RECORD_PATH `
 # Запускаем Python-скрипт
 python "$env:LEADERBOARD_ROOT\leaderboard\leaderboard_evaluator.py" `
   --routes=$env:ROUTES `
-  --routes-subset=$env:ROUTES_SUBSET `
-  --repetitions=$env:REPETITIONS `
   --track=$env:CHALLENGE_TRACK_CODENAME `
   --checkpoint=$env:CHECKPOINT_ENDPOINT `
   --agent=$env:TEAM_AGENT `
   --agent-config="" `
   --debug=$env:DEBUG_CHALLENGE `
-  --record=$env:RECORD_PATH `
   --resume=$env:RESUME
